d0a4bb2d4c4f8da20509f24a3a366a77a841d2f2
Joe Conaty <jconaty@commerce-architects.com>
2020-10-26 18:16:11 -0700

    Adding superpower property to DB

--- backend/database/migrations/20201019172720-add-super-power.js
+++ backend/database/migrations/20201019172720-add-super-power.js
@@ -3,7 +3,7 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.addColumn(
       'Users',
-      'superpower',
+      'souperpower',
       Sequelize.STRING
     )
   }
